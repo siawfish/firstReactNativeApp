5608595f950a5abdc2fa49696375d9e62b415f7e
Gerald <mcamanor@gmail.com>
2020-04-18 12:46:58 +0000

    Added image

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('./assets/avatar.jpg')} style={styles.image}/>
     </View>
   );
 }
@@ -16,4 +16,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  image: {
+    width: 150,
+    height: 150,
+    borderRadius:75,
+  }
 });
